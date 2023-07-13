a294395c9218abbece0dfe2c06ce3d7136a2ce8e
Daniel <dtasada@192.168.0.120>
2023-07-13 13:36:51 +0200

    Update with node+npm

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 312587;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
