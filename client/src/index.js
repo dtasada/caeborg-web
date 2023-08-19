60c82024e256c46c7bb7fb965d94505a213417e7
Daniel Tasada <daniel.tasada@icloud.com>
2023-08-19 16:20:43 +0200

    Changed file structure, iframes working

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+function switchFrame(page) {
+	rest_section = document.getElementById("rest-sec");
+	document.getElementById("rest-iframe").remove();
+	new_frame = document.createElement("iframe");
+
+	Object.assign(new_frame, {
+		id: "rest-iframe",
+		src: `pages/${page}.html`,
+		frameBorder: "0",
+		allow: "clipboard-read; clipboard-write"
+	});
+
+	rest_section.appendChild(new_frame);
+}
