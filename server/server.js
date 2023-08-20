a50cd2f897895c9595ea97e583c520c7238c5e4e
Daniel Tasada <daniel.tasada@icloud.com>
2023-08-20 23:11:20 +0200

    Migrated to SCSS

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
-require('dotenv').config();
-const express = require("express");
+const http = require('http');
+const fs = require('fs')
 
-const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from server!" });
+const server = http.createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	fs.readFile('client/public/index.html', function(error, data) {
+		if (error) {
+			res.writeHead(404);
+			res.write('Error: File Not Found');
+		} else {
+			res.write(data);
+		}
+		res.end();
+	});
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('Server listening on', process.env.PORT);
+server.listen(port, function(error) {
+	if (error) {
+		console.log('Something went wrong', error)
+	} else {
+		console.log(`Server is listening on ${port}`)
+	}
 });
