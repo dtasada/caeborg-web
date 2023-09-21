17a7a2f2d711bfd83ffa9738166930cfb9e6c25a
Daniel Tasada <daniel.tasada@icloud.com>
2023-09-21 17:10:46 +0200

    added shortcut button to html and css, script not done yet

--- client/public/scripts/launcher.js
+++ client/public/scripts/launcher.js
@@ -1,9 +1,19 @@
 import { ourUrl } from '../env.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const new_shortcut_sec = document.getElementById('new-shortcut-sec');
+    const url_input = document.getElementById('url-input');
+    const name_input = document.getElementById('name-input');
+});
+
+document.addEventListener("DOMContentLoaded", () => {
     const li = document.querySelectorAll('#launcher-ol li');
     li.forEach(liElement => {
         const img = liElement.querySelector('img');
         img.setAttribute('src', `${ourUrl}:8080/icon?url=${img.getAttribute('class')}&size=64..128..256`)
     });
 })
+
+function addShortcut() {
+    new_shortcut_sec.hidden = false;
+}
