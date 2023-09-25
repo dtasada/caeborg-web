83e6885b249492efe216c54a56cc728fd4597431
Daniel Tasada <daniel.tasada@icloud.com>
2023-09-25 21:35:11 +0200

    everything working great, quote button is a little off.

--- client/public/scripts/quote.js
+++ client/public/scripts/quote.js
@@ -2,11 +2,21 @@ const quoteUrl = "https://inspirobot.me/api?generate=true";
 
 const quoteButton = document.getElementById("quote-button");
 const img = document.createElement("img");
-quoteButton.addEventListener("click", () => {
+
+quoteButton.addEventListener("click", newQuote);
+function newQuote() {
     fetch(quoteUrl)
         .then(async response => {
             const img_url = await response.text();
             img.src = img_url;
         });
     document.getElementById('images').appendChild(img);
-});
+}
+
+const text = document.getElementById('text');
+window.addEventListener('resize', () => {
+    text.setAttribute('x', text.width/2)
+    text.style.fontSize = text.height;
+})
+
+newQuote();
