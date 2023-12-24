6adee338c907c47afdca979ca6481aa43387d46b
Daniel Tasada <daniel.tasada@icloud.com>
2023-12-24 14:52:17 +0100

    URL FUICKING WORKING

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
 	"fmt"
 	"golang.org/x/net/websocket"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
@@ -28,11 +30,10 @@ func sassFunc() {
 	}
 }
 
-
 func bestIcon() {
 	machineType := runtime.GOOS + "_" + runtime.GOARCH
 
-	besticon := exec.Command("./server/besticon/besticon_" + machineType, ">", "/dev/null")
+	besticon := exec.Command("./server/besticon/besticon_%s" + machineType, ">", "/dev/null")
 	besticon.Env = append(os.Environ(), "PORT=8080", "DISABLE_BROWSE_PAGES=true")
 	besticon.Start()
 }
@@ -58,12 +59,53 @@ func Sock(ws *websocket.Conn) {
 	}
 }
 
-func main() {
-	PATH, _ := os.Getwd()
-	PUBLIC := PATH + "/client/public"
 
-	args := os.Args[1:]
+var tlsConfig = &tls.Config{}
+func setTLS() {
+	tlsConfig.Certificates = make([]tls.Certificate, 1)
+	cert, err := tls.LoadX509KeyPair("/etc/letsencrypt/live/caeborg.dev/fullchain.pem", "/etc/letsencrypt/live/caeborg.dev/privkey.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tlsConfig.Certificates[0] = cert
+}
+
+var customUrl = "https://caeborg.dev"
+var PATH, PUBLIC string
+func startServer() {
+
 	const PORT = 8000
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(PUBLIC)))
+	mux.Handle("/.well-known/acme-challenge/", http.FileServer(http.Dir("/letsencrypt")))
+
+	mux.HandleFunc("/icons*", func (w http.ResponseWriter, r *http.Request) {
+		url := strings.Split(r.URL.Path, "/icons/")[1]
+		http.Redirect(w, r, fmt.Sprintf("%s:8080/icon?%s", customUrl, url), http.StatusSeeOther)
+	})
+
+	// mux.Handle("/chat", websocket.Handler(Sock))
+
+	server := &http.Server{
+		Addr:	fmt.Sprintf(":%d", PORT),
+		Handler:	mux,
+		TLSConfig: tlsConfig,
+	}
+
+	// http.HandleFunc("/chatThings", wsEndpoint)
+
+	// mux.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
+	err := server.ListenAndServeTLS("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	PATH, _ = os.Getwd()
+	PUBLIC = PATH + "/client/public"
+	args := os.Args[1:]
 
 	if len(os.Args[1:]) != 0 {
 		if args[0] == "dev" {
@@ -71,7 +113,6 @@ func main() {
 		}
 	}
 
-	customUrl := "https://caeborg.dev"
 	for _, arg := range args {
 		if strings.Contains(arg, "--url") {
 			customUrl = strings.Split(arg, "=")[1]
@@ -80,18 +121,8 @@ func main() {
 
 	fmt.Printf("At '%s':\n", customUrl)
 
-	http.Handle("/", http.FileServer(http.Dir(PUBLIC)))
-
-	http.HandleFunc("/icons*", func (w http.ResponseWriter, r *http.Request) {
-		url := strings.Split(r.URL.Path, "/icons/")[1]
-		http.Redirect(w, r, fmt.Sprintf("%s:8080/icon?%s", customUrl, url), http.StatusSeeOther)
-	})
-
-	// http.HandleFunc("/chatThings", wsEndpoint)
-
-	http.Handle("/chat", websocket.Handler(Sock))
-
+	setTLS()
 	bestIcon()
+	startServer()
 
-	http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 }
